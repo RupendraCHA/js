d582cf4b34f65c31559ce15318f4745ec6f059fe
RupendraCHA <chandaluri210@gmail.com>
2025-05-25 18:00:30 +0530

    Shadowing Concept

--- Block&GlobalScope/index.js
+++ Block&GlobalScope/index.js
@@ -8,18 +8,29 @@
 // let c = 50
 // console.log(a)
 
-let a = 10
-const b = 20
+// let a = 10
+// const b = 20
+
+// {
+//     var d = 30
+//     let a = 40
+//     const b = 50
+
+//     console.log(a)
+//     console.log(b)
+//     console.log(d)
+// }
+
+// console.log(a)
+// console.log(b)
+
+let a = 30
+
+// let a = 20;
 
 {
-    var d = 30
-    let a = 40
-    const b = 50
-
+    let a = 9
+    // var a = 20
     console.log(a)
-    console.log(b)
-    console.log(d)
 }
-
 console.log(a)
-console.log(b)
