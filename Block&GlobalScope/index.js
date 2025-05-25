9795b482e5bb1a7d40a49e1b4aaf54b12cf4c566
RupendraCHA <chandaluri210@gmail.com>
2025-05-25 09:19:18 +0530

    Block Scope and Global Scope

---
+++ Block&GlobalScope/index.js
@@ -0,0 +1,25 @@
+// {
+//     let a = 10
+//     console.log(a)
+// }
+
+// var b = 10
+
+// let c = 50
+// console.log(a)
+
+let a = 10
+const b = 20
+
+{
+    var d = 30
+    let a = 40
+    const b = 50
+
+    console.log(a)
+    console.log(b)
+    console.log(d)
+}
+
+console.log(a)
+console.log(b)
