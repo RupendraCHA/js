8acb7b26ae86698e90782cdabca1bc99f47688e7
RupendraCHA <chandaluri210@gmail.com>
2025-05-29 14:49:30 +0530

    Working with Function Expression

--- Functions/index.js
+++ Functions/index.js
@@ -1,21 +1,40 @@
 
+console.log("-----------")
+console.log("Function Statement")
 
-var x = 1
-a()
-b()
-console.log(x)
 
 let c = 200
 const d = 320
 
+// Function Statement
 function a(){
     var x = 10
     console.log(x)
 }
 
+
 function b(){
     var x = 100
     console.log(x)
 }
+a()
+b()
 
-console.log(c, d)
+
+// Function Expression
+console.log("-----------")
+console.log("Function Expression")
+
+let x = function (){
+    console.log("This is the Function Expression with Anonymous Function as a Value.")
+}
+
+console.log(x)
+x()
+
+let x1 = function abc(){
+    console.log("This is the Function Expression with Named Function Expression as a Value.")
+}
+
+console.log(x1)
+x1()
