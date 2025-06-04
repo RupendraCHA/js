f6151af99dadbc760c3226afed3b4e403baf551a
RupendraCHA <chandaluri210@gmail.com>
2025-06-04 13:49:56 +0530

    Callback functions with Event Listeners and setTimeout functions

--- CallbacksAndSetTimeout/index.js
+++ CallbacksAndSetTimeout/index.js
@@ -1,13 +1,17 @@
 
-setTimeout(function () {
-    console.log("Timer")
-}, 5000)
+// setTimeout(function () {
+//     console.log("Timer")
+// }, 5000)
 
-function x(y){
-    console.log("x")
-    y()
-}
+// function x(y){
+//     console.log("x")
+//     y()
+// }
 
-x(function y(){
-    console.log("y")
+// x(function y(){
+//     console.log("y")
+// })
+
+document.getElementById("clickMe").addEventListener("click", function xyz(){
+    console.log("Button Clicked.")
 })
