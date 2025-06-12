f9a95ac09ee46833bdac9c9c3bbf9fa0e69f9985
RupendraCHA <chandaluri210@gmail.com>
2025-06-12 09:25:57 +0530

    Changing prototypes

--- PrototypalInheritance/index.js
+++ PrototypalInheritance/index.js
@@ -19,3 +19,27 @@ let object2 = {
 }
 
 object2.__proto__ = object
+
+Function.prototype.myBind = function (){
+    console.log("Random Function")
+}
+
+let fun2 = () => {
+
+}
+
+Array.prototype.city = {
+    name: "Rupendra",
+    age: 25,
+    fatherName: "Chiranjeevi",
+    mother: "Vasundhara"
+}
+
+Object.prototype.object1 = {
+    name: "Rupendra",
+    city: "Ongole",
+    age: 25,
+    isGraduate: function () {
+        console.log(this.name + " " + this.lastname + " completed his graduation")
+    }
+}
