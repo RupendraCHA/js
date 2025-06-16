0cd7518466b4d5321ea216bba92d6845e08868e5
RupendraCHA <chandaluri210@gmail.com>
2025-06-16 08:13:24 +0530

    Creating a Promise Chain for order creation, payment and order Summary

--- Promises/index.js
+++ Promises/index.js
@@ -2,20 +2,12 @@
 
 const cart = ["Jeans", "Shirts", "Electronics"];
 
-function createOrder(){
+function createOrder() {}
 
-}
+function proceedToPayment() {}
 
-function proceedToPayment(){
-
-}
-
-function orderSummary(){
-
-}
-function updateWallet(){
-
-}
+function orderSummary() {}
+function updateWallet() {}
 
 createOrder(cart, function () {
   proceedToPayment(function () {
@@ -28,47 +20,112 @@ createOrder(cart, function () {
 // Async handling using callbacks causes Callback Hell & inversion of control
 // to avoid this problem we use promises to handle async tasks
 
-const promise = createOrder(cart);
+// const promise = createOrder(cart);
 
-console.log(promise)
+// console.log(promise)
 
-promise.then(function (orderId){
-    console.log(orderId)
-})
+// // console.log(promise);
+// function placeOrder(){
+//     try {
+//         promise
+//   .then(function (orderId) {
+//     console.log(orderId);
+//   })
+//   .catch(function (err) {
+//     // alert(err.message)
+//     console.log(err.message);
+//   });
+//     } catch (error) {
+//         console.log(error.message)
+//     }
+// }
 
-// Promise Chain
+// placeOrder()
 
-// promise
-//   .then((paymentInfo) => proceedToPayment(orderId))
-//   .then((orderId) => orderSummary(paymentInfo))
-//   .then((paymentInfo) => updateWallet());
+try {
+    createOrder(cart)
+  .then(function (orderId) {
+    console.log(orderId);
+    return orderId;
+  })
+  .then(function (orderId) {
+    return proceedToPayment(orderId);
+  })
+  .then(function (paymentInfo) {
+    console.log(paymentInfo);
+    return paymentInfo;
+  })
+  .then(function (paymentInfo) {
+    return orderSummary(paymentInfo);
+  })
+  .then(function (orderSummaryData) {
+    console.log(orderSummaryData);
+  })
+  .catch(function (err) {
+    console.log(err.message);
+  });
+} catch (error) {
+    console.log(error)
+}
 
-function validateCart(){
-    return true
+function validateCart(cart) {
+  return true;
+}
+function validateOrderId(orderId) {
+  return false;
+}
+
+function validatePayment(paymentInfo){
+    return false
+}
+
+function createOrder(cart) {
+  return new Promise(function (resolve, reject) {
+    const orderId = {
+      message: true,
+      orderId: "12345",
+    };
+    if (!validateCart(cart)) {
+      const err = Error("Provide valid cart details.");
+
+      return reject(err);
+    } else {
+      setTimeout(function () {
+        resolve(orderId);
+      }, 3000);
+    }
+  });
+}
+function proceedToPayment(orderId) {
+  return new Promise(function (resolve, reject) {
+    if (!validateOrderId(orderId)) {
+      const err = new Error("OrderId is not Valid.");
+
+      return reject(err);
+    } else {
+      setTimeout(function () {
+        resolve("Payment Successful");
+      }, 3000);
+    }
+  });
 }
 
 
-function createOrder(cart) {
-    const pr = new Promise(function (resolve, reject){
-
-        if (!validateCart(cart)){
-            const err = new Error("Provide valid cart details.")
+function orderSummary(paymentInfo){
+    return new Promise(function(resolve, reject){
+        if(!validatePayment(paymentInfo)){
+            const err = new Error("This is not a valid payment")
             reject(err)
-        }
-
-        const orderId = {
-            message: true,
-            orderId: "12345"}
-
-        if (orderId){
-            // console.log(orderId)
-
+        }else{
+            const orderSummary = {
+                totalItems: 5,
+                totalValue: "1,23,780/-",
+                deliveryAddress: "Rudravaram, S.N.Padu, Prakasam, Andhra Pradesh, 523225"
+            }
             setTimeout(function(){
-
-                resolve(orderId)
-            },5000)
+                resolve(orderSummary)
+            }, 3000)
         }
+    
     })
-
-    return pr
 }
