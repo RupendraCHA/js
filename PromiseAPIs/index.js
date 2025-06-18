f8b6989915b976f1fc4ecbb38f62f16327d4536c
RupendraCHA <chandaluri210@gmail.com>
2025-06-19 01:38:53 +0530

    Promise all anyone error scenario

--- PromiseAPIs/index.js
+++ PromiseAPIs/index.js
@@ -1,30 +1,60 @@
 
 // Promise.all()
 
-// All Success
+// ALL SUCCESS
+
+// const allP1 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("P1 is resolved."))
+// })
+
+// const allP2 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("P2 is resolved."))
+// })
+
+// const allP3 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("P3 is resolved."))
+// })
+
+// const allP4 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("P4 is resolved."))
+// })
+
+// const allP5 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("P5 is resolved."))
+// })
+
+// ANYONE ERROR
+
 const allP1 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("P1 is successfully resolved."))
+    setTimeout(() => resolve("P1 is resolved."))
 })
 
 const allP2 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("P2 is successfully resolved."))
+    setTimeout(() => reject("P2 is Rejected."))
 })
 
 const allP3 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("P3 is successfully resolved."))
+    setTimeout(() => resolve("P3 is resolved."))
 })
 
 const allP4 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("P4 is successfully resolved."))
+    setTimeout(() => resolve("P4 is resolved."))
 })
 
 const allP5 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("P5 is successfully resolved."))
+    setTimeout(() => resolve("P5 is resolved."))
 })
 
+
+
 async function runPromiseAll() {
-    const promiseAllResult = await Promise.all([allP1, allP2, allP3, allP4, allP5])
+
+    try {
+        const promiseAllResult = await Promise.all([allP1, allP2, allP3, allP4, allP5])
     console.log(promiseAllResult)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 runPromiseAll()
