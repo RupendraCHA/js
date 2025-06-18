cf5ef32f58b6623cbbeee293f234a82d07ba326a
RupendraCHA <chandaluri210@gmail.com>
2025-06-19 01:51:26 +0530

    Promise all settled in all success calls

--- PromiseAPIs/index.js
+++ PromiseAPIs/index.js
@@ -4,46 +4,46 @@
 // ALL SUCCESS
 
 // const allP1 = new Promise((resolve, reject) => {
-//     setTimeout(() => resolve("P1 is resolved."))
+//     setTimeout(() => resolve("P1 is resolved."), 3000)
 // })
 
 // const allP2 = new Promise((resolve, reject) => {
-//     setTimeout(() => resolve("P2 is resolved."))
+//     setTimeout(() => resolve("P2 is resolved."), 5000)
 // })
 
 // const allP3 = new Promise((resolve, reject) => {
-//     setTimeout(() => resolve("P3 is resolved."))
+//     setTimeout(() => resolve("P3 is resolved."), 4000)
 // })
 
 // const allP4 = new Promise((resolve, reject) => {
-//     setTimeout(() => resolve("P4 is resolved."))
+//     setTimeout(() => resolve("P4 is resolved."), 6000)
 // })
 
 // const allP5 = new Promise((resolve, reject) => {
-//     setTimeout(() => resolve("P5 is resolved."))
+//     setTimeout(() => resolve("P5 is resolved after 8 seconds."), 8000)
 // })
 
 // ANYONE ERROR
 
-const allP1 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("P1 is resolved."))
-})
+// const allP1 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("P1 is resolved."), 3000)
+// })
 
-const allP2 = new Promise((resolve, reject) => {
-    setTimeout(() => reject("P2 is Rejected."))
-})
+// const allP2 = new Promise((resolve, reject) => {
+//     setTimeout(() => reject("P2 is Rejected after 4 seconds."), 4000)
+// })
 
-const allP3 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("P3 is resolved."))
-})
+// const allP3 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("P3 is resolved."), 1000)
+// })
 
-const allP4 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("P4 is resolved."))
-})
+// const allP4 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("P4 is resolved."), 2000)
+// })
 
-const allP5 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("P5 is resolved."))
-})
+// const allP5 = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("P5 is resolved."), 2000)
+// })
 
 
 
@@ -53,8 +53,43 @@ async function runPromiseAll() {
         const promiseAllResult = await Promise.all([allP1, allP2, allP3, allP4, allP5])
     console.log(promiseAllResult)
     } catch (error) {
-        console.log(error)
+        console.error(error)
     }
 }
 
-runPromiseAll()
+// runPromiseAll()
+
+// Promise All Settled
+
+// All resolved/Success
+
+const allP1 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("P1 is resolved."), 3000)
+})
+
+const allP2 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("P2 is resolved."), 4000)
+})
+
+const allP3 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("P3 is resolved."), 1000)
+})
+
+const allP4 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("P4 is resolved."), 2000)
+})
+
+const allP5 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("P5 is resolved."), 5000)
+})
+
+async function runAllSettled() {
+    try {
+        const promiseAllSettledResult = await Promise.allSettled([allP1, allP2, allP3, allP4, allP5])
+        console.log(promiseAllSettledResult)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+runAllSettled()
